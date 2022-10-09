68e275d0fbf08b70ae3fc9df1692ad80e132d36b
Stefan Stölzle <stefan@stoelzle.me>
2022-10-09 14:29:23 +0200

    📄 Update license

--- utils/csv.go
+++ utils/csv.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Stefan Stölzle <stefan@stoelzle.me>
+Copyright © 2022 Stefan Stölzle <stefan@stoelzle.me>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
