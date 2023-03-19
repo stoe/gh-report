fe6e7b018b78ebb527c4780f741dfab2ed54a8fa
Stefan Stölzle <stefan@stoelzle.me>
2023-03-19 16:27:34 +0100

    🔖 v2.1.3

--- cmd/root.go
+++ cmd/root.go
@@ -66,7 +66,7 @@ var (
 		Use:               "gh-report",
 		Short:             "gh cli extension to generate reports",
 		Long:              "gh cli extension to generate enterprise/organization/user/repository reports",
-		Version:           "2.1.2",
+		Version:           "2.1.3",
 		PersistentPreRunE: run,
 	}
 
